ecf8f11e1cb208a5d2742543bc4effc7d744cf45
24f1000999 <24f1000999@ds.study.iitm.ac.in>
2025-11-30 16:28:46 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@ Generates trend plot comparing quarterly efficiency to industry target.
 
 Author/Contact: 24f1000999@ds.study.iitm.ac.in
 """
-# updated for PR
+# updated for PR req
 
 import matplotlib.pyplot as plt
 
